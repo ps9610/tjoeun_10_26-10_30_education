61f78bbad18219302f94507b761d1f92db0fafa1
ps9610 <ps961001@gmail.com>
2020-10-29 08:50:25 +0900

    변경없음

--- 10_28/10_28_프로젝트_스타벅스_섹션_8_혼자하기/js/section8.js
+++ 10_28/10_28_프로젝트_스타벅스_섹션_8_혼자하기/js/section8.js
@@ -4,36 +4,39 @@ var t = 0;
 
 setTimeout(init,100);
 
-function init(){
-    $(".right-row1 .image").stop().animate({ right:-1000 },0).animate({ right:0 },2500);
-    $(".right-row2 .image").stop().animate({ right:-1000 },0).animate({ right:0 },2900);
-}
-
-function formatFn(){
-    $(".right-row1 .image").stop().animate({ right:-1000 },1000);
-    $(".right-row2 .image").stop().animate({ right:-1000 },1000);
-};
-function animateFn(){
-    $(".right-row1 .image").stop().animate({ right:0 },2500);
-    $(".right-row2 .image").stop().animate({ right:0 },2800);
-};
+    function init(){
+        $(".right-row1 .image").stop().animate({ right:-1000 },0).animate({ right:0 },2500);
+        $(".right-row2 .image").stop().animate({ right:-1000 },0).animate({ right:0 },2900);
+    }
 
 
 
-$(window).scroll(function(){
-    
-    if( $(this).scrollTop() >= $("#section7").offset().top+200 ){
-        if( t==0 ){
-            t=1;
-            animateFn();
-        }
-    if( $(this).scrollTop() < $("#section7").offset().top+200 ){
-        if( t==1 ){
-            t=0;
-            formatFn();
+
+    function formatFn(){
+        $(".right-row1 .image").stop().animate({ right:-1000 },1000);
+        $(".right-row2 .image").stop().animate({ right:-1000 },1000);
+    };
+    function animateFn(){
+        $(".right-row1 .image").stop().animate({ right:0 },2500);
+        $(".right-row2 .image").stop().animate({ right:0 },2800);
+    };
+
+
+
+    $(window).scroll(function(){
+
+        if( $(this).scrollTop() >= $("#section7").offset().top+200 ){
+            if( t==0 ){
+                t=1;
+                animateFn();
+            }
+        if( $(this).scrollTop()  < $("#section7").offset().top+200 ){
+            if( t==1 ){
+                t=0;
+                formatFn();
+            }
         }
     }
-}
 
 });
 })(document,window,jQuery);
