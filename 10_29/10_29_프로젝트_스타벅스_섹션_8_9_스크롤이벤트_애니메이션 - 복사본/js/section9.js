90d4de870eba0f42d7323754ed720dd9a8d4fe41
ps9610 <ps961001@gmail.com>
2020-10-30 10:19:13 +0900

    과제

---
+++ 10_29/10_29_프로젝트_스타벅스_섹션_8_9_스크롤이벤트_애니메이션 - 복사본/js/section9.js
@@ -0,0 +1,14 @@
+;(function(document,window,$,undefined){
+    
+    var t=0;
+
+    setTimeout(formatFn,100);
+
+    function formatFn(){
+        $()
+    };
+    function animateFn(){
+
+    };
+
+})(document,window,jQuery);
